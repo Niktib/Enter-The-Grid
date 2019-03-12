f4ea124a3b13d23546bbd1ace6c4a1232b37fc65
Niktib <nikolas.otto.maier@gmail.com>
2019-03-12 10:06:58 -0400

    Changed Agent in TestBEd

--- Niks WIP/gridworldtestbed.py
+++ Niks WIP/gridworldtestbed.py
@@ -10,21 +10,17 @@ class testbed:
 
     def __init__(self, randomSpwan=False):
         self.gridWorld = GridWorld.GridWorld({"grid": 2, "x" : 2 , "y" : 2})
-        if randomSpwan:
-            self.agent = gridAgent.Agent(random.randint(0,4),random.randint(0,4),random.randint(0,len(self.gridWorld.arrayOfGrids)),mc.monteCarlo())
-        else:
-            self.agent = gridAgent.Agent(2,2,3,mc.monteCarlo())
-
         self.gridWorld.pieceItTogether()
 
 
     def run(self, iterations, episodes):
         for iter in range(1, iterations+1):
             for ep in range(1,episodes+1):
+				agent = gridAgent.Agent(random.randint(0,4),random.randint(0,4),random.randint(0,len(self.gridWorld.arrayOfGrids)),mc.monteCarlo())
 				while True:
-					self.gridWorld.printOut(self.agent.agentState)
-					self.agent.results(self.gridWorld.agentMove(self.agent.agentState, self.agent.move))
-					print("Iter: {}, Ep: {}, {}".format(iter, ep, self.gridWorld.agent.playerStatus())) if debug else False #debug
+					self.gridWorld.printOut(agent.agentState)
+					agent.results(self.gridWorld.agentMove(agent.agentState, agent.move))
+					print("Iter: {}, Ep: {}, {}".format(iter, ep, agent.playerStatus())) if debug else False #debug
 					#Check if goal state has been reached?
 					if self.grid.finished:
 						#Reset goal state
@@ -32,7 +28,6 @@ class testbed:
 						break
 				#At end of episode, update policy and grab it from agent to give to new agent
 				policy = agent.policyDetails()
-				self.agent = gridAgent.Agent(random.randint(0,4),random.randint(0,4),random.randint(0,len(self.gridWorld.arrayOfGrids)),mc.monteCarlo())
 
 test = testbed()
 test.run(1, 10000)
--- Niks WIP/sarsa.py
+++ Niks WIP/sarsa.py
@@ -11,4 +11,40 @@ class sarsa:
 		self.stateActionMap = blankStateActionArray
 		
 	def decision(self, state):
+		if random.random() < epsilon:
+			return self.explore(state)
+		else:
+			return self.exploit(state)
 	
+	def exploit(self, state):
+		#contains the values each action has been granted
+		actionArray = []
+		numberOfTimesVisited = []
+		arrayOfActions = self.stateActionMap[state[0]][state[1]][state[2]]
+		for i in range(arrayOfActions):
+			actionArray.append(arrayOfActions[0])
+			numberOfTimesVisited.append(arrayOfActions[1])
+		#First time visit always explore
+		if max(numberOfTimesVisited) == 0: return self.explore()
+		#each index is representative of the action,so 1 = North, 2 = east, etc. 
+		#By finding the max and its index we know the greedy action.
+		return arrayOfActions.index(max(actionArray))
+        
+	def explore(self):
+		#No need to figure out which action in the state, we just need a random action
+		return random.randint(1,self.numOfActions)
+		
+	def updateStates(self, statesTraversed, newState):
+		#statesTraversed is an array in format [grid, x, y, action]
+		statesTraversed.reverse()
+		totalReturn = 0
+		for i in range(statesTraversed)
+			s = statesTraversed[i][0] #State
+			a = statesTraversed[i][1] #Chosen Action
+			r = statesTraversed[i][2] #Reward
+			totalReturn += r + self.gamma * totalReturn 
+			currentValue = self.stateActionMap[s[0]][s[1]][s[2]][a][0]
+			numberOfTimesPicked = self.stateActionMap[s[0]][s[1]][s[2]][a][1]
+			self.stateMap[s[0]][s[1]][s[2]] = 0 #need the update function for state
+			#Qn = Qn + 1/n (Rt + Qn)
+			self.stateActionMap[s[0]][s[1]][s[2]][a] = currentValue + 1/numberOfTimesPicked(totalReturn - currentValue) #need the update function for action picking
